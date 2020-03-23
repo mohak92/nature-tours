c41e5e9e5bc0ff490044d813f13ced16631de22a
mohak92 <tamhane.mohak@gmail.com>
2020-03-23 06:27:13 -0400

    eslint

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const tours = JSON.parse(
 
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
-    status: 'success', 
+    status: 'success',
     results: tours.length,
     data: {
       tours: tours
@@ -24,7 +24,7 @@ app.get('/api/v1/tours/:id', (req, res) => {
   const tour = tours.find(el => el.id === id);
 
   //if(id > tours.length){
-    if(!tour){
+  if (!tour) {
     return res.status(404).json({
       status: 'fail',
       message: 'Invalid Id'
@@ -32,7 +32,7 @@ app.get('/api/v1/tours/:id', (req, res) => {
   }
 
   res.status(200).json({
-    status: 'success', 
+    status: 'success',
     data: {
       tour
     }
@@ -42,21 +42,24 @@ app.get('/api/v1/tours/:id', (req, res) => {
 app.post('/api/v1/tours', (req, res) => {
   //console.log(req.body);
   const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId}, req.body);
+  const newTour = Object.assign({ id: newId }, req.body);
   tours.push(newTour);
-  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-    res.status(201).json({
-      status: 'success',
-      data: {
-        tour: newTour
-      }
-    });
-  });
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    err => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour
+        }
+      });
+    }
+  );
 });
 
 app.patch('/api/v1/tours/:id', (req, res) => {
-
-  if(req.params.id * 1 > tours.length){
+  if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: 'fail',
       message: 'Invalid Id'
@@ -72,8 +75,7 @@ app.patch('/api/v1/tours/:id', (req, res) => {
 });
 
 app.delete('/api/v1/tours/:id', (req, res) => {
-
-  if(req.params.id * 1 > tours.length){
+  if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: 'fail',
       message: 'Invalid Id'
