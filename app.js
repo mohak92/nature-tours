dd5cb5dca45a3c1bedf234c447d18d21568c4a89
mohak92 <tamhane.mohak@gmail.com>
2020-09-08 21:39:28 -0400

    logging using morgan

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-const express = require('express');
 const fs = require('fs');
+const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
+
+// 1) MIDDLEWARE
+app.use(morgan('dev'));
+
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -17,6 +22,8 @@ app.use((req, res, next) => {
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
+
+// 2) ROUTE HANDELERS
 
 const getAllTours = (req, res) => {
   console.log(req.requestTime);
@@ -107,6 +114,8 @@ const deleteTour = (req, res) => {
 //app.patch('/api/v1/tours/:id', updateTour);
 //app.delete('/api/v1/tours/:id', deleteTour);
 
+// 3) ROUTES
+
 app
   .route('/api/v1/tours')
   .get(getAllTours)
@@ -118,6 +127,8 @@ app
   .patch(updateTour)
   .delete(deleteTour);
 
+
+// 4) START SERVER
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
